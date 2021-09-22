7c51502493271bf938782d9fc2c8e69fdc22259c
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2021-09-22 14:12:40 -0500

    Primer commit parte III

--- src/main/java/edu/eci/cvds/servlet/ActualServlet.java
+++ src/main/java/edu/eci/cvds/servlet/ActualServlet.java
@@ -43,7 +43,48 @@ public class ActualServlet extends HttpServlet {
         //En caso que no se pase parametro opcional o no sea numero entero
         catch(NumberFormatException numberFormatException){
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            message = Service.getHTMLError(resp.getStatus(), "Requerimiento Invalido.")
+            message = Service.getHTMLError(resp.getStatus(), "Requerimiento Invalido.");
+        //En caso que haya malformaciones en la URL
+        }
+        catch(MalformedURLException malformedURLException){
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        //En cualquier otro caso de excepcion
+        catch(Exception exception){
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            message = Service.getHTMLError(resp.getStatus(), "Requerimiento Invalido.");
+        }
+        finally{
+            responseWriter.write(message);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        Writer responseWriter = resp.getWriter();
+        String message = "";
+        try{
+            //Se pide el id del ToDo, con esto se pide el ToDo con ese valor
+            int toDoId = Integer.parseInt(req.getParameter("id"));   
+            Todo newTodo = Service.getTodo(toDoId);
+            //Si el item existe, se da una respuesta de OK
+            resp.setStatus(HttpServletResponse.SC_OK);
+            //Se crea una lista donde se almacena los ToDo encontrados y se
+            //retornan como una tabla HTML 
+            ArrayList<Todo> todoArray = new ArrayList<>();
+            todoArray.add(newTodo);
+            message = Service.todosToHTMLTable(todoArray);
+            System.out.print(message);
+        }
+        //En caso de que el parametro del identificador no tenga nada en el o no se encuentre
+        catch(FileNotFoundException fileNotFoundException){
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            message = Service.getHTMLError(resp.getStatus(), "No existe Item con el identificador dado");
+        }
+        //En caso que no se pase parametro opcional o no sea numero entero
+        catch(NumberFormatException numberFormatException){
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            message = Service.getHTMLError(resp.getStatus(), "Requerimiento Invalido.");
         //En caso que haya malformaciones en la URL
         }
         catch(MalformedURLException malformedURLException){
