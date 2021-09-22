86f3809a8f695d2cb4b5fe0217a60ed9ccd00efb
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2021-09-21 22:10:44 -0500

    Commit segunda parte

---
+++ src/main/java/edu/eci/cvds/servlet/ActualServlet.java
@@ -0,0 +1,49 @@
+package edu.eci.cvds.servlet;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Writer;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
+import edu.eci.cvds.servlet.model.Todo;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.NotFoundException;
+
+//URL del servidor 
+@WebServlet(urlPatterns = {"/ActualServlet"})
+
+public class ActualServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        Writer responseWriter = resp.getWriter();
+        String message = "";
+        try{
+            //Se pide el id del ToDo, con esto se pide el ToDo con ese valor
+            int toDoId = Integer.parseInt(req.getParameter("id"));   
+            Todo newTodo = Service.getTodo(toDoId);
+            //Si el item existe, se da una respuesta de OK
+            resp.setStatus(HttpServletResponse.SC_OK);
+            //Se crea una lista donde se almacena los ToDo encontrados y se
+            //retornan como una tabla HTML 
+            ArrayList<Todo> todoArray = new ArrayList<>();
+            todoArray.add(newTodo);
+            message = Service.todosToHTMLTable(todoArray);
+        }
+        catch(FileNotFoundException fileNotFoundException){
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            message = Service.getHTMLError();
+
+        }
+        catch(MalformedURLException malformedURLException){
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        finally{
+
+        }
+    }
+}
